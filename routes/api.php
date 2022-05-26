7178ab87cdae7a0f93d804e4d023222746dc1f18
Jesse <jesse.andrade@prf.gov.br>
2022-05-26 10:27:07 -0300

    Your branch is up to date with 'origin/master'. first commit
    	modified:   .devcontainer/devcontainer.json
    	modified:   .devcontainer/docker-compose.yml
    	modified:   .gitignore
    	new file:   app/Http/Controllers/Api/CategoryController.php
    	new file:   app/Http/Controllers/Api/GenreController.php
    	new file:   app/Models/Category.php
    	new file:   app/Models/Genre.php
    	new file:   app/Models/Traits/Uuid.php
    	modified:   app/User.php
    	modified:   composer.json
    	modified:   composer.lock
    	new file:   database/factories/CategoryFactory.php
    	new file:   database/factories/GenreFactory.php
    	new file:   database/migrations/2022_05_25_181717_create_categories_table.php
    	new file:   database/migrations/2022_05_26_112459_create_genres_table.php
    	new file:   database/seeds/CategorySeeder.php
    	modified:   database/seeds/DatabaseSeeder.php
    	new file:   database/seeds/GenreSeeder.php
    	modified:   routes/api.php
    	modified:   routes/web.php

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'Api'], function () {
+    Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
+    Route::resource('genres', GenreController::class, ['except' => ['create', 'edit']]);
+});
