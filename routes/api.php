51ca7b64a05d97ab0056012e06b8665489168fd2
Jesse <jesse.andrade@prf.gov.br>
2022-06-06 22:41:23 -0300

    Changes to be committed: 	modified:   app/Models/Category.php 	modified:   app/Models/Genre.php 	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'Api'], function () {
-    Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
-    Route::resource('genres', GenreController::class, ['except' => ['create', 'edit']]);
+    $exceptCreateAndEdit = ['except' => ['create', 'edit']];
+    Route::resource('categories', CategoryController::class, $exceptCreateAndEdit);
+    Route::resource('genres', GenreController::class, $exceptCreateAndEdit);
 });
